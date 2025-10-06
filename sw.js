151443d2f93a52b837c84b4dab851bc316f3e593
CoachBJ <coachbiggiejames@gmail.com>
2025-10-05 21:33:53 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,45 +1,29 @@
-// Simple PWA cache for offline use
-const CACHE = 'scoring-v33';
+const CACHE = 'scoring-v50';
 const ASSETS = [
   './',
   './index.html',
   './style.css',
   './app.js',
-  './manifest.webmanifest'
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png',
+  './icon-180.png'
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(self.skipWaiting())
-  );
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
 
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.map(k => (k === CACHE ? null : caches.delete(k))))
-    ).then(() => self.clients.claim())
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
   );
 });
 
 self.addEventListener('fetch', (e) => {
-  const { request } = e;
-  if (request.method !== 'GET') return;
   e.respondWith(
-    caches.match(request).then((cached) =>
-      cached ||
-      fetch(request).then((resp) => {
-        const respClone = resp.clone();
-        caches.open(CACHE).then((cache) => {
-          // Only cache same-origin GETs
-          try {
-            if (request.url.startsWith(self.location.origin)) {
-              cache.put(request, respClone);
-            }
-          } catch(_) {}
-        });
-        return resp;
-      }).catch(() => caches.match('./index.html'))
-    )
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
