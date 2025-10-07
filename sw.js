19d58ed1d6ee2384c821b33c88115554cd6d60a0
CoachBJ <coachbiggiejames@gmail.com>
2025-10-07 13:59:15 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const CACHE = 'scoring-v56';
+const CACHE = 'scoring-v57';
 const ASSETS = [
   './',
   './index.html',
@@ -7,7 +7,7 @@ const ASSETS = [
   './app.js',
   './manifest.webmanifest',
   // keep these ONLY if the files exist at the app root:
-  './White C.png',
+  './white-c.png',
   './icon-512.png',
   './icon-180.png',
 ];
